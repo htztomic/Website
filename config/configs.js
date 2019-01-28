8440499519e3ec491dbe2bf36bf9b4d3da029d1e
HaiTao Zhang <haitaozhang246@gmail.com>
2019-01-28 03:33:22 -0800

    package require fix

---
+++ config/configs.js
@@ -0,0 +1,6 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb+srv://chaosgear:gearshed@chaos-tht8o.mongodb.net/gear_shed?retryWrites=true'
+  //db_dev: 'mongodb://localhost:27017/login_demo',
+};
--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -1,5 +1,5 @@
-const User = require('../../models/User')
-const UserSession = require('../../models/UserSession')
+const User = require('chaosWebsite/client/server/models/models/User')
+const UserSession = require('chaosWebsite/client/server/models/models/UserSession')
 
 
 function validateEmail(email) {
